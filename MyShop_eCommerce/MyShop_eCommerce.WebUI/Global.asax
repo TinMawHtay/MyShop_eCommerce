80f59f97135fc3b864c01d96d59127a3bf2c9dae
Yi Yi Po <yiyipo91@gmail.com>
2019-08-24 13:16:47 +0800

    //Initial Comment

---
+++ MyShop_eCommerce/MyShop_eCommerce.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShop_eCommerce.WebUI.MvcApplication" Language="C#" %>
---
+++ MyShop_eCommerce/MyShop_eCommerce.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop_eCommerce.WebUI.Startup))]
+namespace MyShop_eCommerce.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
